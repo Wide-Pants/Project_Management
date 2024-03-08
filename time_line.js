2d73398b276afbaf11d36d53b9636be0c89f6f31
Wide-Pants <ajtwoddl1236@naver.com>
2024-03-09 02:01:47 +0900

    20240308

--- time_line.js
+++ time_line.js
@@ -1,55 +1,97 @@
 const scheduler = document.getElementById(`schedule`)
 
 const today_btn = document.getElementById(`today`)
-const dates_btn = document.getElementById(`dates`)
-const months_btn = document.getElementById(`months`)
-const seasons_btn = document.getElementById(`seasons`)
+const week_view_btn = document.getElementById(`week_view_btn`)
+const month_view_btn = document.getElementById(`month_view_btn`)
+const season_view_btn = document.getElementById(`season_view_btn`)
 
-mag_status = `dates`
+const weeks = document.getElementById(`weeks`);
+const months = document.getElementById(`months`);
+const seasons = document.getElementById(`seasons`);
 
-dates_btn.addEventListener(`click`,()=>{
-    if(mag_status!=`dates`){
-        const dates = document.getElementsByClassName(`dates`);
-        Array.from(dates).forEach(element => {
-        element.style.fontSize = `14px`;
-        });
-        if(mag_status!=`months`){
-            const months = document.getElementsByClassName(`months`);
-            Array.from(months).forEach(element => {
-            element.style.fontSize = `14px`;
-        });
-        }
+const add_plan_line = document.getElementById(`add_plan_line`)
+const add_new_plan_btn = document.getElementById(`add_plan_btn`)
+const input_new_plan = document.getElementById(`plan_type`)
+
+var mag_status = `week`
+const selected_color = `#e6ffea`;
+
+const project_abbreviation = `PM` // project_manage
+var cnt = 0;
+
+
+week_view_btn.addEventListener(`click`,()=>{
+    if(mag_status!=`week`){
+        weeks.style.display = `flex`;
+        months.style.display = `none`;
+        seasons.style.display = `none`;
+        week_view_btn.style.backgroundColor=`selected_color`;
+        month_view_btn.style.backgroundColor=`none`;
+        season_view_btn.style.backgroundColor=`none`;
+        mag_status=`week`;
     }
-    mag_status=`dates`;
 });
 
-months_btn.addEventListener(`click`,()=>{
+month_view_btn.addEventListener(`click`,()=>{
     if(mag_status!=`months`){
-        const dates = document.getElementsByClassName(`dates`);
-        Array.from(dates).forEach(element => {
-        element.style.fontSize = `0px`;
-        });
-    }if(mag_status==`seasons`){
-        const months = document.getElementsByClassName(`months`);
-        Array.from(months).forEach(element => {
-            element.style.fontSize = `14px`;
-        });
+        weeks.style.display = `none`;
+        months.style.display = `flex`;
+        seasons.style.display = `none`;
+        month_view_btn.style.backgroundColor=`selected_color`;
+        week_view_btn.style.backgroundColor=`none`;
+        season_view_btn.style.backgroundColor=`none`;
+        mag_status=`months`;
     }
-    mag_status=`months`;
 });
 
-seasons_btn.addEventListener(`click`,()=>{
+season_view_btn.addEventListener(`click`,()=>{
     if(mag_status!=`seasons`){
-        if(mag_status==`dates`){
-            const dates = document.getElementsByClassName(`dates`);
-            Array.from(dates).forEach(element => {
-                element.style.fontSize = `0px`;
-            });
-        }
-        const months = document.getElementsByClassName(`months`);
-        Array.from(months).forEach(element => {
-            element.style.fontSize = `0px`;
-        });
+        weeks.style.display = `none`;
+        months.style.display = `none`;
+        seasons.style.display = `flex`;
+        season_view_btn.style.backgroundColor=`selected_color`;
+        month_view_btn.style.backgroundColor=`none`;
+        week_view_btn.style.backgroundColor=`none`;
+        mag_status=`seasons`;
     }
-    mag_status=`seasons`;
 });
+
+add_new_plan_btn.addEventListener(`click`,()=>{
+    input_new_plan.style.display=`block`
+    add_new_plan_btn.style.fontSize = `0`
+    input_new_plan.focus();
+})
+
+input_new_plan.addEventListener(`keydown`,(e)=>{
+    console.log(e.code)
+    if(e.code ==`NumpadEnter` || e.code ==`Enter`){
+        const plan_ID_value = input_new_plan.value;
+        console.log(plan_ID_value);
+        input_new_plan.blur();
+        input_new_plan.value = ``;
+        add_plan(plan_ID_value);
+        add_new_plan_btn.classList.toggle(`odd_status`)
+        add_plan_line.classList.toggle(`odd_status`)
+    }
+})
+
+input_new_plan.addEventListener(`focusout`,()=>{
+    input_new_plan.style.display=`none`
+    add_new_plan_btn.style.fontSize = `16px`
+})
+
+
+function add_plan(plan_name){
+    const new_plan = document.createElement(`div`);
+    const new_plan_bar = document.createElement(`div`)
+    new_plan_bar.setAttribute(`id`,'_'+plan_name)
+    new_plan_bar.classList.add(`plan_bar`);
+    new_plan.classList.add(`plan_name`);
+    new_plan.innerHTML = `🅰️<span class="project_abbreviation">${project_abbreviation}-${cnt}</span>${plan_name}`
+    if(add_new_plan_btn.classList.contains(`odd_status`)){
+        new_plan_bar.classList.add(`odd_status`);
+        new_plan.classList.add(`odd_status`);
+    }
+    add_new_plan_btn.before(new_plan);
+    add_plan_line.before(new_plan_bar);
+}

2218e09097202c2265060555a886d2e5f630a53b
Wide-Pants <ajtwoddl1236@naver.com>
2024-03-10 02:56:31 +0900

    20240309

--- time_line.js
+++ time_line.js
@@ -14,7 +14,7 @@ const add_new_plan_btn = document.getElementById(`add_plan_btn`)
 const input_new_plan = document.getElementById(`plan_type`)
 
 var mag_status = `week`
-const selected_color = `#e6ffea`;
+const selected_color = `#d2ffd9`;
 
 const project_abbreviation = `PM` // project_manage
 var cnt = 0;
@@ -63,10 +63,8 @@ add_new_plan_btn.addEventListener(`click`,()=>{
 })
 
 input_new_plan.addEventListener(`keydown`,(e)=>{
-    console.log(e.code)
     if(e.code ==`NumpadEnter` || e.code ==`Enter`){
         const plan_ID_value = input_new_plan.value;
-        console.log(plan_ID_value);
         input_new_plan.blur();
         input_new_plan.value = ``;
         add_plan(plan_ID_value);
@@ -83,10 +81,11 @@ input_new_plan.addEventListener(`focusout`,()=>{
 
 function add_plan(plan_name){
     const new_plan = document.createElement(`div`);
-    const new_plan_bar = document.createElement(`div`)
-    new_plan_bar.setAttribute(`id`,'_'+plan_name)
-    new_plan_bar.classList.add(`plan_bar`);
+    const new_plan_bar = document.createElement(`div`);
+    new_plan.setAttribute(`id`,'_'+plan_name+`_`);
+    new_plan_bar.setAttribute(`id`,'_'+plan_name);
     new_plan.classList.add(`plan_name`);
+    new_plan_bar.classList.add(`plan_bar`);
     new_plan.innerHTML = `🅰️<span class="project_abbreviation">${project_abbreviation}-${cnt}</span>${plan_name}`
     if(add_new_plan_btn.classList.contains(`odd_status`)){
         new_plan_bar.classList.add(`odd_status`);
@@ -94,4 +93,89 @@ function add_plan(plan_name){
     }
     add_new_plan_btn.before(new_plan);
     add_plan_line.before(new_plan_bar);
+    new_plan.addEventListener(`mouseover`, ()=>{
+        new_plan.classList.add(`hover_status`)
+        new_plan_bar.classList.add(`hover_status`)
+    })
+    new_plan_bar.addEventListener(`mouseover`, ()=>{
+        new_plan.classList.add(`hover_status`)
+        new_plan_bar.classList.add(`hover_status`)
+    })
+    
+    new_plan.addEventListener(`mouseout`, ()=>{
+        new_plan.classList.remove(`hover_status`)
+        new_plan_bar.classList.remove(`hover_status`)
+    })
+    new_plan_bar.addEventListener(`mouseout`, ()=>{
+        new_plan.classList.remove(`hover_status`)
+        new_plan_bar.classList.remove(`hover_status`)
+    })
 }
+
+
+function load_date_info(){
+    const today = new Date();
+    
+    const start_date = new Date();
+    start_date.setFullYear(today.getFullYear()-1)
+    start_date.setDate(1);
+    start_date.setDate(start_date.getDate()-start_date.getDay()+1)
+
+    const end_date = new Date();
+    end_date.setFullYear(today.getFullYear()+1)
+    end_date.setDate(1);
+    end_date.setDate(end_date.getDate()-5+end_date.getDay())
+
+    console.log(start_date, end_date)
+
+    const s_start = new Date(`${today.getFullYear()-1}-01-01`)
+    
+    for(var i=0; i<12; i++){
+        const new_element = document.createElement(`div`);
+
+        new_element.classList.add(`season`);
+        new_element.innerText = s_start.getFullYear().toString().substring(2)+`' ` + (i%4+1) + `분기 \n ${(s_start.getMonth()+1)+`~`+(s_start.getMonth()+3)}`
+        s_start.setMonth(s_start.getMonth()+3);
+
+        seasons.appendChild(new_element);
+    }
+
+    const m_start = new Date(`${today.getFullYear()-1}-${today.getMonth()+1}-01`);
+
+    for(var i=0; i<36; i++){
+        const new_element = document.createElement(`div`);
+
+        new_element.classList.add(`month`);
+        new_element.innerText = m_start.getFullYear().toString().substring(2) == today.getFullYear().toString().substring(2)? (m_start.getMonth()+1) + `월`: m_start.getFullYear().toString().substring(2)+`' ` + (m_start.getMonth()+1) + `월`
+        m_start.setMonth(m_start.getMonth()+1);
+
+        months.appendChild(new_element);
+    }
+
+    while(start_date < end_date){
+        const new_element = document.createElement(`div`);
+        new_element.classList.add(`week`);
+
+        const month_container = document.createElement(`div`);
+        const dates_container = document.createElement(`div`);
+
+        for(var i = 0; i < 7; i++){
+            const date = document.createElement(`div`);
+            date.innerText = start_date.getDate();
+            date.classList.add(`date`);
+            console.log(start_date.getDate())
+            start_date.setDate(start_date.getDate()+1);
+            if(i>4)
+                date.classList.add(`weekend`);
+            dates_container.appendChild(date);
+        }
+
+        month_container.innerText = innerText = start_date.getFullYear().toString().substring(2) == today.getFullYear().toString().substring(2)? (start_date.getMonth()+1) + `월`: start_date.getFullYear().toString().substring(2)+`' ` + (start_date.getMonth()+1) + `월`
+        console.log(start_date.getMonth()+1);
+        new_element.appendChild(month_container);
+        new_element.appendChild(dates_container);
+        weeks.append(new_element);
+    }
+}
+
+load_date_info()
